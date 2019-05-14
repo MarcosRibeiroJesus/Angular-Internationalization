0b3c207a41ff4c1643af566b3c6e26108b116a7e
MarcosRibeiroJesus <marcoz.iu@gmail.com>
2019-05-14 18:08:13 -0300

    ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngxTranslate';
+  title = 'ngx-translate';
+
+  constructor(private translateService: TranslateService) {
+    translateService.setDefaultLang('pt-br');
+  }
+
+  switchLanguage(language: string) {
+    this.translateService.use(language);
+  }
 }
